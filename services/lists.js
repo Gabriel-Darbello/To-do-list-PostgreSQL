d41b8331d829ce1640d1b54ddcb8190767090e51
Gabriel Darbello de Oliveira <gadarbello@gmail.com>
2026-04-04 17:28:29 -0300

    adição da função delete list
    adição da função delete list que pega o id da lista como parametro e deleta ele do database

--- services/lists.js
+++ services/lists.js
@@ -12,3 +12,15 @@ export async function createList(nome, descricao){
     console.error('Erro ao inserir', err.stack);
   }
 }
+
+export async function deleteList(id){
+  const query = ('DELETE FROM lista WHERE id = $1')
+  const values = [id]
+  try {
+    const res = await pool.query(query, values)
+    console.log('Lista deletada: ', res.rows[0])
+  }
+  catch(err) {
+    console.log('Erro ao deletar', err.stack)
+  }
+}
