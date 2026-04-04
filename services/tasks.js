83a4926303e93979c39fdee2c15d18ed8c692259
Gabriel Darbello de Oliveira <gadarbello@gmail.com>
2026-04-04 20:50:09 -0300

    feat: criação de CRUD para tarefas
    foi criada as funções para manipulação de tarefas sendo elas createTask deleteTask updateTask e getTask

---
+++ services/tasks.js
@@ -0,0 +1,52 @@
+import { pool } from "./db_connect.js";
+
+export async function createTask(nome, descricao, listaID) {
+  const query = ('INSERT INTO tarefas (nome, descricao, lista_id) VALUES ($1, $2, $3) RETURNING *');
+  const values = [nome, descricao, listaID];
+
+  try {
+    const res = await pool.query(query, values)
+    console.log('Lista inserida:', res.rows[0]);
+  }
+  catch (err) {
+    console.error('Erro ao inserir', err.stack);
+  }
+}
+
+export async function deleteTask(id) {
+  const query = ('DELETE FROM tarefas WHERE id = $1');
+  const values = [id];
+
+  try {
+    const res = await pool.query(query, values)
+    console.log('Lista deletada:', res.rows[0]);
+  }
+  catch (err) {
+    console.error('Erro ao deletar', err.stack);
+  }
+}
+
+export async function updateTask(nome, descricao, concluida , id) {
+  const query = ('UPDATE tarefas SET nome = $1, descricao = $2, concluida = $3 WHERE id = $4 RETURNING *');
+  const values = [nome, descricao, concluida, id];
+
+  try {
+    const res = await pool.query(query, values)
+    console.log('Lista atualizada:', res.rows[0]);
+  }
+  catch (err) {
+    console.error('Erro ao atualizar', err.stack);
+  }
+}
+
+export async function getTask(listaID) {
+  const query = ('SELECT * FROM tarefas WHERE lista_id = $1')
+  const values = [listaID]
+  try {
+    const res = await pool.query(query, values)
+    console.log('Tarefas do banco: ', res.rows)
+  }
+  catch(err) {
+    console.log('Erro ao pegar tarefas', err.stack)
+  }
+}
