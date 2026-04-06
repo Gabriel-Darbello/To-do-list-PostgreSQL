73924538b21d02aa3d988595fa8c5555ad56ac7d
Gabriel-Darbello <gadarbello@gmail.com>
2026-04-06 20:56:26 -0300

    fix: valor padrão definido para tarefas

--- schema.sql
+++ schema.sql
@@ -8,6 +8,6 @@ CREATE TABLE tarefas (
   id SERIAL PRIMARY KEY,
   nome TEXT NOT NULL,
   descricao TEXT,
-  concluida BOOLEAN NOT NULL ,
+  concluida BOOLEAN NOT NULL DEFAULT false,
   lista_id INTEGER NOT NULL REFERENCES lista(id) ON DELETE CASCADE
 );
